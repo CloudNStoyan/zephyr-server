30864653eddcda603e15237caf2b13f9255e4da1
Stoyan <slownessbg@gmail.com>
2022-01-12 01:07:20 +0200

    starting the base styling

--- Notes/NoteService.cs
+++ Notes/NoteService.cs
@@ -16,7 +16,7 @@ namespace Zephyr.Notes
         public async Task<NotePoco[]?> GetNotes(int offset = 0, int limit = 20)
         {
             var notePocos = await this.Database.Query<NotePoco>(
-                "SELECT * FROM note OFFSET @offset LIMIT @limit;",
+                "SELECT * FROM note ORDER BY created ASC OFFSET @offset LIMIT @limit;",
                 new NpgsqlParameter("offset", offset),
                 new NpgsqlParameter("limit", limit)
                 );
