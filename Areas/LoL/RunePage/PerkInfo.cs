72bfba3b205f407e45978ace1f0ad4ad757683b6
Stoyan <slownessbg@gmail.com>
2022-01-16 19:21:53 +0200

    adding styling to all runepages page, and a few global css fixes

---
+++ Areas/LoL/RunePage/PerkInfo.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Zephyr.Areas.LoL.RunePage;
+
+public class PerkInfo
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string? Name { get; set; }
+
+    [JsonProperty("icon")]
+    public string? Icon { get; set; }
+}
---
+++ Areas/LoL/RunePage/PerkService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace Zephyr.Areas.LoL.RunePage
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class PerkService
+    {
+        private IWebHostEnvironment Env { get; }
+
+        public PerkInfo[] Perks { get; set; } = Array.Empty<PerkInfo>();
+
+        public PerkService(IWebHostEnvironment env)
+        {
+            this.Env = env;
+
+            this.LoadPerks();
+        }
+
+        private void LoadPerks()
+        {
+            string perksPath = Path.Combine(this.Env.WebRootPath, "js/lol/perks.json");
+
+            if (!File.Exists(perksPath))
+            {
+                throw new Exception($"Can't find file at: '{perksPath}'");
+            }
+
+            string perkJson = File.ReadAllText(perksPath);
+
+            var perks = JsonConvert.DeserializeObject<PerkInfo[]>(perkJson);
+
+            if (perks == null)
+            {
+                throw new Exception($"Failed to deserialize '{perksPath}' as '{nameof(PerkInfo)}'");
+            }
+
+            const string iconPathPrefix = "/static/lol/";
+
+            foreach (var perkInfo in perks)
+            {
+                perkInfo.Icon = iconPathPrefix + perkInfo.Icon;
+            }
+
+            this.Perks = perks;
+        }
+    }
+}
